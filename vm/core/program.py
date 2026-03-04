f0a2d4b487940ab164371d162215db8676a78c5c
Иванов Владислав <ivanov.vlad135@gmail.com>
2026-03-04 23:22:08 +0500

    format with ruff

--- vm/core/program.py
+++ vm/core/program.py
@@ -1,5 +1,6 @@
 from core.instruction import Instruction
 
+
 class Program:
     def __init__(self, instructions: list[Instruction]):
         self.instructions = instructions
@@ -9,10 +10,10 @@ class Program:
         if index < 0 or index >= len(self.instructions):
             raise IndexError("Instruction index out of bounds.")
         self._current_instruction_index = index
-    
+
     def next_instruction(self):
         self.jump_to_instruction(self._current_instruction_index + 1)
-    
+
     def get_current_instruction(self) -> Instruction:
         if self._current_instruction_index >= len(self.instructions):
             raise IndexError("No more instructions to execute.")
--- vm/main.py
+++ vm/main.py
@@ -1,11 +1,13 @@
 from core.runtime import VirtualMachine
 from core.program import Program
 
+
 def main():
 
     vm = VirtualMachine()
     program = Program([])
     vm.run_program(program)
+
 
 if __name__ == "__main__":
     main()
