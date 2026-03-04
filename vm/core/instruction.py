2041c9d86c5013ec79afbc652d76578f8d2aa0ce
Иванов Владислав <ivanov.vlad135@gmail.com>
2026-03-04 23:22:00 +0500

    add classes for commands

--- vm/core/instruction.py
+++ vm/core/instruction.py
@@ -1,3 +1,65 @@
 class Instruction:
     def __init__(self):
         pass
+
+
+class Push(Instruction):
+    def __init__(self, segment, index):
+        super().__init__()
+        self.segment = segment
+        self.index = index
+
+
+class Pop(Instruction):
+    def __init__(self, segment, index):
+        super().__init__()
+        self.segment = segment
+        self.index = index
+
+
+class Arithmetic(Instruction):
+    pass
+
+
+class Add(Arithmetic):
+    pass
+
+
+class Sub(Arithmetic):
+    pass
+
+
+class Neg(Arithmetic):
+    pass
+
+
+class Logical(Instruction):
+    pass
+
+
+class And(Logical):
+    pass
+
+
+class Or(Logical):
+    pass
+
+
+class Not(Logical):
+    pass
+
+
+class Comparison(Instruction):
+    pass
+
+
+class Eq(Comparison):
+    pass
+
+
+class Gt(Comparison):
+    pass
+
+
+class Lt(Comparison):
+    pass
